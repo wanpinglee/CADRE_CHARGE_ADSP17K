15d15a4350697555da22e8aa431c7af778326f55
wanpinglee <wanping.lee@gmail.com>
2021-11-30 20:00:17 -0500

    Added scripts/2_multi_var.sh

---
+++ scripts/2_multi_var.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]
+then
+      echo "usage: $0 <VCF> <REF> <OUT_DIR>"
+      exit 1
+fi
+
+VCF=$1
+REF=$2
+OUT_DIR=$3
+
+mkdir -p $OUT_DIR
+
+# AN > 1690 since we like to filter variants if their GT missing > 0.05.
+# bin/compact_vcf reads from stdin and generates to stdout
+
+bin/bcftools filter -S . -i "FMT/GQ >= 20 & FMT/DP >= 10" $VCF \
+  | bin/compact_vcf \
+  | bin/bcftools norm -f $REF -D -d all -m- \
+  | bin/bcftools view -v snps -f 'PASS' \
+  | bin/bcftools +fill-tags - -- -t AC,AN,AF \
+  | bin/bcftools view -i "AN > 1690" -Oz -o $OUT_DIR/$(basename $VCF)
