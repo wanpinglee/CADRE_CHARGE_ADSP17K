abc2a6f161321e6b0068d454173637d59cb638db
Wan-Ping Lee <wan-ping.lee@pennmedicine.upenn.edu>
2022-06-28 13:08:07 +0000

    Remove hard coded paths in 2_multi_var.sh

--- scripts/2_multi_var.sh
+++ scripts/2_multi_var.sh
@@ -13,11 +13,10 @@ SAMPLES=$3
 OUT_DIR=$4
 
 mkdir -p $OUT_DIR
+SCRIPT_DIR=$(dirname "$0")
 
-cd /mnt/data3/old-master/leew/tools/callset_preparation/scripts
-
-../bin/bcftools view --threads 2 -S $SAMPLES -Ou -f PASS $VCF \
-  | ../bin/bcftools annotate --threads 2 -Ou -x "FORMAT/PGT,FORMAT/PID,FORMAT/PL,FORMAT/PS" \
-  | ../bin/bcftools norm --threads 2 -m -any -f $REF -O u \
-  | ../bin/bcftools filter --threads 2 -S . -Ou -i "FMT/GQ >= 20 & FMT/DP >= 10" \
-  | ../bin/bcftools annotate --threads 2 -x "FORMAT" -Oz -o $OUT_DIR/$(basename $VCF)
+$SCRIPT_DIR/../bin/bcftools view --threads 2 -S $SAMPLES -Ou -f PASS $VCF \
+  | $SCRIPT_DIR/../bin/bcftools annotate --threads 2 -Ou -x "FORMAT/PGT,FORMAT/PID,FORMAT/PL,FORMAT/PS" \
+  | $SCRIPT_DIR/../bin/bcftools norm --threads 2 -m -any -f $REF -O u \
+  | $SCRIPT_DIR/../bin/bcftools filter --threads 2 -S . -Ou -i "FMT/GQ >= 20 & FMT/DP >= 10" \
+  | $SCRIPT_DIR/../bin/bcftools annotate --threads 2 -x "FORMAT" -Oz -o $OUT_DIR/$(basename $VCF)
